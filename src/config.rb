c795d20890429a9c5bdcea643bb5a35a3a710e24
洪满义 <1459829655@qq.com>
2020-01-10 21:38:00 +0800

    1月10日提交

--- src/config.rb
+++ src/config.rb
@@ -3,13 +3,13 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "../dist/stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded 
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
